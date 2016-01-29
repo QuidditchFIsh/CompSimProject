70c6fd4bc0a0acd1961552f87bad345c5c88017d
QuidditchFIsh <nye42@live.co.uk>
2016-01-29 15:38:23 +0000

    things

--- src/Function.java
+++ src/Function.java
@@ -1,12 +1,14 @@
 
-public class Function {
+public class Function
+{
 
 	
-	public void arrayUpdatePosition(Particle3D[] position, double dt, Vector3D[][] forceArray) {
-		Vector3D totalForce;
-		for (Particle3D a : position) {
-		
+	public static Vector3D arrayUpdatePosition(Particle3D[] position, double dt, Vector3D[][] forceArray) {
+		Vector3D totalForce= new Vector3D();
+		for (Particle3D a : position) 
+		{
 			
 		}
+		return totalForce;
 	}
 }
