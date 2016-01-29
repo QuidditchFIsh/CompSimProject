9d7ca68e9ed95f0b3a312f72fdb7c52ba4278b9b
Sara <Sara@TriSaratops.Home>
2016-01-29 14:01:39 +0000

    help

--- src/Function.java
+++ src/Function.java
@@ -4,7 +4,7 @@ public class Function {
 	
 	public void arrayUpdatePosition(Particle3D[] position, double dt, Vector3D[][] forceArray) {
 		Vector3D totalForce;
-		for (Particle3D a : position) {
+		for (Particle3D a : position) {7system
 		
 			
 		}
