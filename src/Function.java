2a5d7155295f0ec474a7ce91d378d0ca627a72f1
Sara <Sara@TriSaratops.Home>
2016-01-29 18:51:48 +0000

    Started arrayTotalEnergy method

--- src/Function.java
+++ src/Function.java
@@ -7,12 +7,11 @@ public class Function {
 				Vector3D totalForce = new Vector3D();
 				//Create a Vector3D array to keep a running total of the sum of the forces: the final element represents the total force on one particle
 				Vector3D tempForce[] = new Vector3D[forceArray.length];
-				int j = 0;
 				//Set the 0th element of the array to the 0th element of the ith row in the force array
 				tempForce[0] = forceArray[i][0];
 				
 				//Loop over rows of force matrix to calculate total force on a particle
-				for (j = 0; j < forceArray.length - 1; j++) {
+				for (int j = 0; j < forceArray.length - 1; j++) {
 					//Set the total force to the sum of the jth element of tempForce (i.e. the sum of all previous elements of the force array) and the next element of the force array
 					totalForce = Vector3D.vectorAddition(tempForce[j],forceArray[i][j+1]);
 					//Set the (j+1)th element of tempForce to the sum of forces 0 to j of the force array
@@ -45,6 +44,22 @@ public class Function {
 		}
 	}
 	
+	public double arrayTotalEnergy(Particle3D[] energy) {
+		int i;
+		double totalKinetic;
+		double[] tempKinetic = new double[energy.length];
+		tempKinetic[0] = energy[0].kineticEnergy();
+		for (i = 0; i < energy.length - 1; i++) {
+			totalKinetic = tempKinetic[i] + energy[i+1].kineticEnergy();
+			tempKinetic[i+1] = totalKinetic;
+		}
+		//Only returning this to stop eclipse from complaining
+		return tempKinetic[energy.length];
+	}
+	
+	
+	
+	
+	
+	
 }
-
-
